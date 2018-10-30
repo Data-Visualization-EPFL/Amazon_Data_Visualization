c72628066d77fcdc82e4799a58a1a16f2eff9123
Tim Nguyen <ntim.bugs@gmail.com>
2018-10-30 15:14:38 +0100

    Add toggle styles

--- js/script.js
+++ js/script.js
@@ -19,18 +19,14 @@ class Map {
 
 		this.container.append(svg.firstChild)
 	}
+
 	async getMap() {
 		const response = await fetch("maps/AOI_percountry.svg");
 		return await response.text();
 	}
 }
 
-new Map($("#map"));
-
-
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("Start of the page");
-
-  alert("You will see a Data visualisation project about: Natural Resilience in the Amazon.\
-  You will be able to interact with the data by using the toolbox.")
+	new Map($("#map"));
 });
