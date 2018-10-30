a2e6d4ac084d4d73c74b93236c384da1c6edc3ec
Tim Nguyen <ntim.bugs@gmail.com>
2018-10-30 14:08:36 +0100

    Render a basic map

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-function whenDocumentLoaded(action) {
-	if (document.readyState === "loading") {
-		document.addEventListener("DOMContentLoaded", action);
-	} else {
-		// `DOMContentLoaded` already fired
-		action();
-	}
+function $(sel) {
+  return document.querySelector(sel);
+}
+
+function $$(sel) {
+  return document.querySelectorAll(sel);
 }
 
 
-whenDocumentLoaded(() => {
-  console.log("Start of the page");
+class Map {
+	constructor(container) {
+		this.container = container;
+		this.render();
+	}
+
+	async render() {
+		const parser = new DOMParser();
+		const svg = parser.parseFromString(await this.getMap(), "image/svg+xml");
+
+		this.container.append(svg.firstChild)
+	}
+	async getMap() {
+		const response = await fetch("maps/AOI_percountry.svg");
+		return await response.text();
+	}
+}
+
+new Map($("#map"));
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("Start of the page");
 
   alert("You will see a Data visualisation project about: Natural Resilience in the Amazon.\
   You will be able to interact with the data by using the toolbox.")
