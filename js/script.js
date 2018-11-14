fa58de14a8f047d234b51f053c8a8e79e6227da7
Tim Nguyen <ntim.bugs@gmail.com>
2018-11-14 13:41:35 +0100

    Some experiments with data+map

--- js/script.js
+++ js/script.js
@@ -7,26 +7,70 @@ function $$(sel) {
 }
 
 
-class Map {
-	constructor(container) {
-		this.container = container;
-		this.render();
-	}
+class RealMap {
+  constructor(container) {
 
-	async render() {
-		const parser = new DOMParser();
-		const svg = parser.parseFromString(await this.getMap(), "image/svg+xml");
+      // var vector = new ol.layer.Vector({
+      //   source: new ol.source.Vector({
+      //     url: 'maps/ch-cantons.json',
+      //     format: new ol.format.TopoJSON({
+      //       // don't want to render the full world polygon (stored as 'land' layer),
+      //       // which repeats all countries
+      //       layers: ['countries']
+      //     }),
+      //     overlaps: false
+      //   }),
+      //   // style: style
+      // });
 
-		this.container.append(svg.firstChild)
-	}
-
-	async getMap() {
-		const response = await fetch("maps/AOI_percountry.svg");
-		return await response.text();
-	}
+      var vector = new ol.layer.Vector({
+        source: new ol.source.Vector({
+          url: 'maps/test.geojson',
+          format: new ol.format.GeoJSON({
+            // don't want to render the full world polygon (stored as 'land' layer),
+            // which repeats all countries
+            layers: ['cantons']
+          }),
+          overlaps: false
+        }),
+        // style: style
+      });
+    new ol.Map({
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        }),
+        vector
+      ],
+      target: container.id,
+      view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+      })
+    });
+  }
 }
+
+// class Map {
+// 	constructor(container) {
+// 		this.container = container;
+// 		this.render();
+// 	}
+//
+// 	async render() {
+// 		const parser = new DOMParser();
+// 		const svg = parser.parseFromString(await this.getMap(), "image/svg+xml");
+//
+// 		this.container.append(svg.firstChild)
+// 	}
+//
+// 	async getMap() {
+// 		const response = await fetch("maps/AOI_percountry.svg");
+// 		return await response.text();
+// 	}
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("Start of the page");
-	new Map($("#map"));
+	new RealMap($("#map"));
 });
