c5d00c4e35c9d6ac728e69cca729c3128f699c69
Pierre-Assemat <assematpierrecpe@gmail.com>
2018-11-28 21:05:42 +0100

    Add color settings for each layers
    Select also the "good" categories to work with. The others are irrelevant.

--- js/script.js
+++ js/script.js
@@ -27,32 +27,105 @@ const LAYER_MAP_NAME = {
   'sabana-humedales': 'data/land-use/sabana_humedales.geojson',
   'suela-desnudo': 'data/land-use/suela_desnudo.geojson',
   'thb-df': 'data/land-use/thb_df.geojson',
-  'urbano': 'data/land-use/hurbano.geojson',
-  'vegetacion': 'data/land-use/vegetacio.geojson'
+  'urbano': 'data/land-use/urbano.geojson',
+  'vegetacion': 'data/land-use/vegetacion.geojson'
 };
 
 const LAYER_MAP = {
-  'AOI-percountry': 'data/AOI_percountry.geojson',
-  'land-use-contour': 'data/land-use-contour.geojson',
-  'agricultura': 'data/land-use/agricultura.geojson',
-  'agua': 'data/land-use/agua.geojson',
-  'aguajales': 'data/land-use/aguajales.geojson',
-  'arbustos': 'data/land-use/arbustos.geojson',
-  'bosque-inundable': 'data/land-use/bosque_inundable.geojson',
-  'bosque-no-inundable': 'data/land-use/bosque_no_inundable.geojson',
-  'herbazal': 'data/land-use/herbazal.geojson',
-  'humedales': 'data/land-use/humedales.geojson',
-  'mineria': 'data/land-use/mineria.geojson',
-  'other': 'data/land-use/other.geojson',
-  // 'pasto': 'data/land-use/pasto.geojson',
-  // 'pasto-agricultura': 'data/land-use/pasto_agricultura.geojson',
-  // 'pasto-herbazal': 'data/land-use/pasto_herbazal.geojson',
+  'AOI-percountry': {
+    'url': 'data/AOI_percountry.geojson',
+    'style': null
+  },
+  'agricultura': {
+    'url': 'data/land-use/agricultura.geojson',
+    'style': {
+        color: 'rgba(180, 180, 30, 1)'
+      }
+  },
+  'agua': {
+    'url': 'data/land-use/agua.geojson',
+    'style': {
+        color: 'rgba(20, 20, 240, 1)'
+      }
+  },
+  'aguajales': {
+    'url': 'data/land-use/aguajales.geojson',
+    'style': {
+        color: 'rgba(5, 5, 120, 1)'
+      }
+  },
+  'bosque-inundable': {
+    'url': 'data/land-use/bosque_inundable.geojson',
+    'style': {
+        color: 'rgba(40, 130, 190, 1)'
+      }
+  },
+  'bosque-no-inundable': {
+    'url': 'data/land-use/bosque_no_inundable.geojson',
+    'style': {
+        color: 'rgba(80, 130, 80, 1)'
+      }
+  },
+  'herbazal': {
+    'url': 'data/land-use/herbazal.geojson',
+    'style': {
+        color: 'rgba(8, 230, 3, 1)'
+      }
+  },
+  'mineria': {
+    'url': 'data/land-use/mineria.geojson',
+    'style': {
+        color: 'rgba(0, 0, 0, 1)'
+      }
+  },
+  'pasto': {
+    'url': 'data/land-use/pasto.geojson',
+    'style': {
+        color: 'rgba(80, 30, 30, 1)'
+      }
+  },
+  'pasto-agricultura': {
+    'url': 'data/land-use/pasto_agricultura.geojson',
+    'style': {
+        color: 'rgba(150, 200, 50, 1)'
+      }
+  },
+  'pasto-herbazal': {
+    'url': 'data/land-use/pasto_herbazal.geojson',
+    'style': {
+        color: 'rgba(80, 130, 130, 1)'
+      }
+  },
+  'suela-desnudo': {
+    'url': 'data/land-use/suela_desnudo.geojson',
+    'style': {
+        color: 'rgba(180, 130, 30, 1)'
+      }
+  },
+  'urbano': {
+    'url': 'data/land-use/urbano.geojson',
+    'style': {
+        color: 'rgba(120, 120, 130, 1)'
+      }
+  },
+  'vegetacion': {
+    'url': 'data/land-use/vegetacion.geojson',
+    'style': {
+        color: 'rgba(5, 90, 5, 1)'
+      }
+  },
+  // 'arbustos': {
+  //   'url': 'data/land-use/arbustos.geojson',
+  //   'style': {
+  //       color: 'rgba(5, 90, 5, 1)'
+  //     }
+  // },
+  // 'land-use-contour': 'data/land-use-contour.geojson',
+  // 'humedales': 'data/land-use/humedales.geojson',
   // 'sabana': 'data/land-use/sabana.geojson',
   // 'sabana-humedales': 'data/land-use/sabana_humedales.geojson',
-  // 'suela-desnudo': 'data/land-use/suela_desnudo.geojson',
   // 'thb-df': 'data/land-use/thb_df.geojson',
-  // 'urbano': 'data/land-use/hurbano.geojson',
-  // 'vegetacion': 'data/land-use/vegetacio.geojson'
+  // 'other': 'data/land-use/other.geojson',
 };
 const BASE_ID = "base";
 
@@ -79,15 +152,30 @@ class RealMap {
   }
 
   addLayer(id) {
-    const vector = new ol.layer.Vector({
-      source: new ol.source.Vector({
-        url: LAYER_MAP[id],
-        format: new ol.format.GeoJSON({ layers: ['cantons'] }),
-        overlaps: true
-      }),
-      // style: style
-    });
-    this.layers[id] = vector;
+    console.log(LAYER_MAP[id]);
+    if (LAYER_MAP[id].style == null) {
+      console.log(LAYER_MAP[id]);
+      const vector = new ol.layer.Vector({
+        source: new ol.source.Vector({
+          url: LAYER_MAP[id].url,
+          format: new ol.format.GeoJSON({ layers: ['cantons'] }),
+          overlaps: true
+        })
+      });
+      this.layers[id] = vector;
+    } else {
+      const vector = new ol.layer.Vector({
+        source: new ol.source.Vector({
+          url: LAYER_MAP[id].url,
+          format: new ol.format.GeoJSON({ layers: ['cantons'] }),
+          overlaps: true
+        }),
+        style: new ol.style.Style({
+          fill: new ol.style.Stroke(LAYER_MAP[id].style)
+        })
+      });
+      this.layers[id] = vector;
+    }
   }
 
   toggleLayer(id) {
@@ -118,6 +206,59 @@ class RealMap {
       }),
     });
   }
+
+  AddCluster() {
+    const distance = document.getElementById('distance');
+
+    var count = 20000;
+       var features = new Array(count);
+       var e = 4500000;
+       for (var i = 0; i < count; ++i) {
+         var coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
+         features[i] = new ol.Feature(new ol.geom.Point(coordinates));
+       }
+
+    const source = new ol.source.VectorSource({
+            features: features
+          });
+
+          var clusterSource = new ol.source.Cluster({
+            distance: parseInt(distance.value, 10),
+            source: source
+          });
+
+          var styleCache = {};
+          var clusters = new ol.layer.VectorLayer({
+            source: clusterSource,
+            style: function(feature) {
+              var size = feature.get('features').length;
+              var style = styleCache[size];
+              if (!style) {
+                style = new ol.style.Style({
+                  image: new ol.style.CircleStyle({
+                    radius: 10,
+                    stroke: new ol.style.Stroke({
+                      color: '#fff'
+                    }),
+                    fill: new ol.style.Fill({
+                      color: '#3399CC'
+                    })
+                  }),
+                  text: new ol.style.Text({
+                    text: size.toString(),
+                    fill: new ol.style.Fill({
+                      color: '#fff'
+                    })
+                  })
+                });
+                styleCache[size] = style;
+              }
+              return style;
+            }
+          });
+          this.layers.push({"cluster": clusters});
+          this.map.render();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -126,6 +267,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   createImputList($("#categories"));
   const map = new RealMap($("#map"));
+  map.AddCluster();
 });
 
 function createImputList(el) {
